d2a3f812233da4ecd3b62d75a2d497cde3bf1bec
alamiin <alamiin345@gmail.com>
2026-06-20 10:59:59 +0600

    Initial Commit

---
+++ MovieApp-ios/MovieApp_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieApp_iosApp.swift
+//  MovieApp-ios
+//
+//  Created by Md Al-Amin on 6/20/26.
+//
+
+import SwiftUI
+
+@main
+struct MovieApp_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
